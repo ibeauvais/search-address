f8b9a890043117d9e4f5967f4e770bd268bc071a
ivan <ibeauvais@xebia.fr>
2017-05-16 21:43:58 +0200

    package at fat jar

--- src/main/java/fr/perso/searchaddress/App.java
+++ src/main/java/fr/perso/searchaddress/App.java
@@ -1,15 +1,36 @@
 package fr.perso.searchaddress;
 
-import fr.perso.searchaddress.api.AddressApi;
-import io.vertx.core.Vertx;
 
-public class App {
+import fr.perso.searchaddress.service.SearchService;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.client.WebClient;
 
-    public static void main(String[] args) {
+public class App extends AbstractVerticle {
 
-        Vertx vertx = Vertx.vertx();
+    @Override
+    public void start() {
 
-        vertx.deployVerticle(new AddressApi());
+        HttpServer server = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+        WebClient client = WebClient.create(vertx);
 
+        SearchService searchService = new SearchService("localhost", 9200, client);
+
+        router.get("/api/address/search/:searchText")
+                .handler(routingContext -> {
+                    String searchText = routingContext.request().getParam("searchText");
+                    searchService.searchWord(searchText, result ->
+                            routingContext.response()
+                                    .putHeader("content-type", "application/json; charset=utf-8")
+                                    .end(Json.encode(result))
+                    );
+                });
+
+        server.requestHandler(router::accept)
+                .listen(8080);
     }
+
 }
