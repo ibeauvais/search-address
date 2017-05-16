c08cab8d3b481330c0a1cf2ebfbb421d74f61f62
ivan <ibeauvais@xebia.fr>
2017-05-16 21:52:58 +0200

    Revert "package at fat jar"
    This reverts commit f8b9a890043117d9e4f5967f4e770bd268bc071a.

--- src/main/java/fr/perso/searchaddress/App.java
+++ src/main/java/fr/perso/searchaddress/App.java
@@ -1,36 +1,15 @@
 package fr.perso.searchaddress;
 
+import fr.perso.searchaddress.api.AddressApi;
+import io.vertx.core.Vertx;
 
-import fr.perso.searchaddress.service.SearchService;
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.json.Json;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.client.WebClient;
+public class App {
 
-public class App extends AbstractVerticle {
+    public static void main(String[] args) {
 
-    @Override
-    public void start() {
+        Vertx vertx = Vertx.vertx();
 
-        HttpServer server = vertx.createHttpServer();
-        Router router = Router.router(vertx);
-        WebClient client = WebClient.create(vertx);
+        vertx.deployVerticle(new AddressApi());
 
-        SearchService searchService = new SearchService("localhost", 9200, client);
-
-        router.get("/api/address/search/:searchText")
-                .handler(routingContext -> {
-                    String searchText = routingContext.request().getParam("searchText");
-                    searchService.searchWord(searchText, result ->
-                            routingContext.response()
-                                    .putHeader("content-type", "application/json; charset=utf-8")
-                                    .end(Json.encode(result))
-                    );
-                });
-
-        server.requestHandler(router::accept)
-                .listen(8080);
     }
-
 }
