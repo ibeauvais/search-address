475f227c78051cd7cefbf301405c774c00698de0
ivan <ibeauvais@xebia.fr>
2017-05-17 07:33:38 +0200

    java 9 branch

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module search.address {
+
+    requires jackson.annotations;
+    requires vertx.core;
+    requires vertx.web.client;
+    requires vertx.web.common;
+    requires vertx.web;
+    requires slf4j.api;
+}
